c7fa925bee56722af9375a75bf575d5d504fd2a5
Fera <feraferaca@gmail.com>
2026-01-06 18:11:21 +0300

    Day01 completed

--- day01/main.js
+++ day01/main.js
@@ -1,2 +1,36 @@
-console.log("Day 1: OK")
-console.log(new Date())
+console.log("Day 1: OK");
+console.log(new Date());
+
+let name = "Kolyan";
+let age = 19;
+let isStudent = true;
+let city = "Moscow";
+
+console.log(typeof name, typeof age, typeof isStudent, typeof city);
+
+console.log(age >= 18 ? "yeah" : "nope");
+
+let numbers = [3, 1, 7, 2];
+let sum = 0;
+
+for (let number of numbers) {
+  sum += number;
+}
+
+console.log(sum, Math.max(...numbers));
+
+let user = {
+  name,
+  age,
+};
+
+user.skills = ["C++", "C#", "JS", "Python", "Java", "Kotlin"];
+
+let arr = [];
+
+for (let value of Object.values(user)) {
+  console.log(value);
+  arr.push(String(value));
+}
+
+console.log(arr.join(","));
