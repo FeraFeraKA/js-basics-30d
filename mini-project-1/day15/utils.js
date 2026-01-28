fa3011ab47f2d36353b97a2753fffbd43f2b6fae
Fera <feraferaca@gmail.com>
2026-01-28 18:03:36 +0300

    Deploying notes

--- mini-project-1/day15/utils.js
+++
@@ -1,6 +0,0 @@
-export function getStatus(status) {
-  if (status === "idle") return ""
-  if (status === "loading") return "Загрузка..."
-  if (status === "error") return "Произошла ошибка: "
-  if (status === "success") return "Загрузка завершена"
-}
